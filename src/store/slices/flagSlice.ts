01ef257154185627640efc1125d03e0b24bdd88f
AleksandrFROLik <nextfrolik@gmail.com>
2022-08-18 22:46:07 +0300

    add directory slices+

--- src/store/slices/flagSlice.ts
+++ src/store/slices/flagSlice.ts
@@ -1,11 +1,16 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createSlice, PayloadAction } from '@reduxjs/toolkit';
+import { ResponseGetFlagsType } from '../../api/flagsCountriesAPI';
 
 type FlagType = {
-
+  loading: boolean
+  flags: ResponseGetFlagsType []
+  error: string
 }
 
 const initialState: FlagType = {
-
+  loading: false,
+  flags: [],
+  error: ''
 }
 
 
@@ -13,7 +18,18 @@ export const flagsSlice = createSlice({
   name: 'flags',
   initialState,
   reducers: {
+    fetching(state) {
+      state.loading = true
+    },
+    fetchSuccess(state, action:PayloadAction) {
+      state.loading = false
 
+
+    },
+    fetchError(state, action:PayloadAction<Error>) {
+      state.loading = false
+      state.error = action.payload.message
+    }
   }
 })
 
