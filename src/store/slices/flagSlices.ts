a2464ca149e716a206c1019f2d1caaa84b6c5145
AleksandrFROLik <nextfrolik@gmail.com>
2022-09-09 22:14:17 +0300

    correct work app+

---
+++ src/store/slices/flagSlices.ts
@@ -0,0 +1,20 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+type FlagType = {
+
+}
+
+const initialState: FlagType = {
+
+}
+
+
+export const flagsSlice = createSlice({
+  name: 'flags',
+  initialState,
+  reducers: {
+
+  }
+})
+
+export default flagsSlice.reducer
