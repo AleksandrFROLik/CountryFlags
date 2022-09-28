7be0796e565caed777042389e198b3c657728915
AleksandrFROLik <nextfrolik@gmail.com>
2022-09-28 12:15:38 +0300

    End+

--- src/config.ts
+++ src/config.ts
@@ -1,4 +1,4 @@
 export const BASE_URL = 'https://restcountries.com/v2/';
 export const ALL_COUNTRIES =  'all?fields=name,capital,flags,population,region';
- export const searchByCountry =  BASE_URL + 'name/1';
+export const searchByCountry =  BASE_URL + 'name/';
 export const filterByCode =  BASE_URL + 'alpha?codes=';
