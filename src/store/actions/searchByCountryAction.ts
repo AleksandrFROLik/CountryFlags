4656eb7c6a293a9903695d5befd5fe120cd627ee
AleksandrFROLik <nextfrolik@gmail.com>
2022-12-06 13:05:41 +0300

    End+++

--- src/store/actions/searchByCountryAction.ts
+++ src/store/actions/searchByCountryAction.ts
@@ -8,14 +8,14 @@ import { searchByCountry } from '../../config';
 export const searchByCountryAction = (name: string | undefined) => {
   return async (dispatch: AppDispatch) => {
     try {
-      dispatch(flagCountryAppSlice.actions.fetching)
-      const response = await instance.get<ResponseCountryType[]>(searchByCountry + name)
+      dispatch(flagCountryAppSlice.actions.fetching);
+      const response = await instance.get<ResponseCountryType[]>(searchByCountry + name);
       dispatch(flagCountryAppSlice.actions.fetchSearch({
         country: response.data[0],
-      }))
+      }));
 
     } catch (e) {
-      dispatch(flagCountryAppSlice.actions.fetchError(e as Error))
+      dispatch(flagCountryAppSlice.actions.fetchError(e as Error));
     }
-  }
-}
+  };
+};
