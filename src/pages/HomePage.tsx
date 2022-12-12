b3f482d0af27b99f8fc7449c298232b4a5d5c9f6
AleksandrFROLik <nextfrolik@gmail.com>
2022-12-12 20:57:45 +0300

    End-+

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -10,59 +10,46 @@ import { ResponseGetFlagsType } from '../models/models';
 
 export const HomePage = React.memo(() => {
 
-  const flagsCountry = useAppSelector(state => state.reducer.flagsCountry)
-  const navigate = useNavigate()
-  const dispatch = useAppDispatch()
-  const [filteredCountries, setFilteredCountries] = useState<ResponseGetFlagsType[]>(flagsCountry)
+  const flagsCountry = useAppSelector(state => state.reducer.flagsCountry);
+  const navigate = useNavigate();
+  const dispatch = useAppDispatch();
+  const [filteredCountries, setFilteredCountries] = useState<ResponseGetFlagsType[]>(flagsCountry);
 
   useEffect(() => {
 
     if (!flagsCountry.length) {
-      dispatch(fetchFlagsCountries())
+      dispatch(fetchFlagsCountries());
     }
-  }, [dispatch, flagsCountry])
+  }, [dispatch, flagsCountry]);
 
   const handleSearch = useCallback((search: string, region: RegionType | null) => {
     let data = [...flagsCountry];
 
-    if (region) data = data.filter(c => c.region.includes(region.value))
-    if (search) data = data.filter(c => c.name.toLowerCase().includes(search.toLowerCase()))
+    if (region) data = data.filter(c => c.region.includes(region.value));
+    if (search) data = data.filter(c => c.name.toLowerCase().includes(search.toLowerCase()));
 
-    setFilteredCountries(data)
-  }, [setFilteredCountries, flagsCountry])
+    setFilteredCountries(data);
+  }, [setFilteredCountries, flagsCountry]);
 
-  const navigateToDetails = (name: string) => navigate(`/country/${name}`)
+  const navigateToDetails = (name: string) => navigate(`/country/${ name }`);
 
-  return (
-    <>
-      <Controls onSearch={handleSearch}/>
+  return (<>
+      <Controls onSearch={ handleSearch }/>
       <List>
-        {filteredCountries.map(country => {
-            const countryInfo = {
-              img: country.flags.png,
-              name: country.name,
-              info: [
-                {
-                  title: 'Population',
-                  description: country.population.toLocaleString()
-                },
-                {
-                  title: 'Region',
-                  description: country.region
-                },
-                {
-                  title: 'Capital',
-                  description: country.capital
-                }
-              ]
-            }
-            return <Card key={country.name}
-                         countryInfo={countryInfo}
-                         handleOnClick={navigateToDetails}/>
-          }
-        )
-        }
+        { filteredCountries.map(country => {
+          const countryInfo = {
+            img: country.flags.png, name: country.name, info: [{
+              title: 'Population', description: country.population.toLocaleString()
+            }, {
+              title: 'Region', description: country.region
+            }, {
+              title: 'Capital', description: country.capital
+            }]
+          };
+          return <Card key={ country.name }
+                       countryInfo={ countryInfo }
+                       handleOnClick={ navigateToDetails }/>;
+        }) }
       </List>
-    </>
-  );
+    </>);
 });
